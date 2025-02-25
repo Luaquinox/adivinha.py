91bcd9b8504327bce4d6192a914af7145459d90e
Luaquinox <160145426+Luaquinox@users.noreply.github.com>
2025-02-25 10:56:50 -0300

    Add files via upload

--- adivinha.py
+++ adivinha.py
@@ -2,23 +2,29 @@ print("**************************")
 print("Bem vindo Jogo da Adivinha")
 print("**************************")
 
-
-chute = int(input("Digitem o seu número: "))
 numero_secreto = 19
-acertou = chute == numero_secreto
-chute_maior = chute > numero_secreto
-chute_menor = chute < numero_secreto
+total_de_tentativas = 3
+rodada = 1
 
-print(f"Você digitou {chute}")
+while (rodada <= total_de_tentativas):
+   print(f"Tentativa {rodada} de {total_de_tentativas} ")
+   chute = int(input("Digitem o seu número: "))
 
-if(acertou):
-   print("Você acertou!")
-else:
- if (chute_maior):
-  print("Você errou! seu número foi maior que número secreto")
- if ( chute_menor):
-    print("Você errou! seu número foi menor que número secreto")
+   acertou = chute == numero_secreto
+   chute_maior = chute > numero_secreto
+   chute_menor = chute < numero_secreto
 
+   print(f"Você digitou {chute}")
+
+   if(acertou):
+      print("Você acertou!")
+   else:
+      if (chute_maior):
+         print("Você errou! seu número foi maior que número secreto")
+      if (chute_menor):
+         print("Você errou! seu número foi menor que número secreto")
+
+   rodada = rodada + 1
 
 print("@@@@@@@@@@@")
 print("Fim de jogo")
